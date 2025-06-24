5761e93e9af7626fddea63c9ba23a6951555369d
Saja <sajaabdelkhaliq@gmail.com>
2025-06-24 17:48:20 +0300

    add documentation for home page

--- Rayyan/Rayyan_Saja/src/test/java/test/HomePage.java
+++ Rayyan/Rayyan_Saja/src/test/java/test/HomePage.java
@@ -14,6 +14,11 @@ public class HomePage {
     WebDriver driver;
     WebDriverWait wait;
 
+    /**
+     * Constructor to initialize the HomePage with a WebDriver instance.
+     *
+     * @param driver The WebDriver instance to interact with the browser.
+     */
     public HomePage(WebDriver driver) {
         this.driver = driver;
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
@@ -21,13 +26,20 @@ public class HomePage {
 
     By homeIconLocater = By.cssSelector("a[aria-label='Home']");
 
+    /**
+     * Verifies that the Home icon is visible on the page.
+     */
     public void checkVisibilityOfHomeIcon() {
         WebElement homeIcon = wait.until(ExpectedConditions.visibilityOfElementLocated(homeIconLocater));
         checkVisibilityOfElement(homeIcon);
     }
 
+    /**
+     * Asserts that the given WebElement is displayed on the page.
+     *
+     * @param element The WebElement to verify visibility for.
+     */
     public void checkVisibilityOfElement(WebElement element) {
         Assert.assertTrue(element.isDisplayed(), "Element is not displayed");
     }
-
 }
